96b27897a8c4a9f5a46c49ac6a600e8028ef88c2
mhsndd <2653136986@qq.om>
2021-12-12 19:16:51 +0800

    棋盘初始

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,10 +1,17 @@
-function renderbBoard(){
+function renderbBoard(numRows,numCols){
     let boardEl = document.querySelector("#board");
 
-    for (let i=0;i< NumRows;i++){
-        
+    for(let i=0;i<numRows;i++){
+        let trEl=document.createElement("tr");
+        for (let j=0; j< numCols;j++){
+            let trEl=document.createElement("td");
+            let cellEl=document.createElement("div");
+            tdEl.append(cellEl)
+            
+            trEl.append(trEl)
+        }
+        boardEl.append(trEl)
     }
 
 }
-
 renderbBoard();
