c340c7552c1205e864f7fcd05abe0a8be4ccdd0b
mhsndd <2653136986@qq.om>
2021-12-16 09:18:21 +0800

    改动前

--- Minesweeper.js
+++ Minesweeper.js
@@ -6,7 +6,29 @@ function renderBoard(numRows,numCols,grid) {
         for (let j=0; j< numCols;j++ ){
             let cellEl =document.createElement("div");
             cellEl.className="cell";
-            cellEl.innerText =grid[i][j];
+            grid[i][j].cellEl=cellEl
+
+            //if (grid[i][j].count === -1){
+            //    cellEl.innerText="*";
+            //}else{
+            //    cellEl.innerText=grid[i][j].count;
+            //}
+            
+            cellEl.addEventListener("click",(e)=>{
+                if (grid[i][j].count ===-1 ){
+                    exploded(grid, i, j, numRows ,numCols );
+                    return;
+                }
+                if (grid[i][j].count === 0){
+                    searchClearArea(grid,i ,j,numRows,numCols);
+                }else if (grid [i][j].count>0 ){
+                    grid[i][j].clear= true;
+                    cellEl.classList.add("clear");
+                    grid[i][j].cellEl.innerText =grid[i][j].count
+                }
+                checkAllClear(grid)
+                //cellEl.classList.add("clear");
+            })
 
             
             let tdEl =document.createElement("td");
@@ -30,7 +52,10 @@ function initialize(numRows,numCols,numMines){
     for (let i =0; i < numRows; i++){
         grid[i]=new Array(numCols);
         for (let j =0; j < numCols; j++){
-            grid[i][j]=0;
+            grid[i][j]={
+                clear: false,
+                count: 0
+            };
         }
 
     }
@@ -42,36 +67,37 @@ function initialize(numRows,numCols,numMines){
         let col =cellSn % numCols;
 
 
-        grid[row][col]= -1;
+        grid[row][col].count= -1;
         mines.push([row ,col]);
         
     }
     
       //计算有雷周边为零的周边雷数
     for (let [row,col] of mines) {
-        //console.log("")
+        //
+        console.log("")
         for (let [drow,dcol] of directions) {
             let cellRow = row + drow;
             let cellCol = col + dcol;
             if (cellRow <0 || cellRow >= numRows || cellCol <0 || cellCol >= numCols){
                 continue;
             }
-            if (grid[cellRow][cellCol] ==0){
+            if (grid[cellRow][cellCol].count ==0){
                 let count = 0;
                 for (let [arow,acol] of directions){
-                    let ambientRow= row + arow;
-                    let ambientCol= col + acol;
+                    let ambientRow= cellRow + arow;
+                    let ambientCol= cellCol + acol;
                     if (ambientRow <0 || ambientRow >= numRows || ambientCol <0 || ambientCol >= numCols){
                         continue;
                     }
 
-                    if (grid[ambientRow][ambientCol] ==-1){
+                    if (grid[ambientRow][ambientCol].count ==-1){
                         count +=1;
                     }
                 }
                 
                 if (count > 0 ) {
-                    grid[cellRow][cellCol] = count;
+                    grid[cellRow][cellCol].count = count;
                 }
             
             }
@@ -85,7 +111,74 @@ function initialize(numRows,numCols,numMines){
     return grid;
 }
 
+function searchClearArea(grid ,row,col,numRows,numCols){
+    let gridCell =grid[row][col];
+    gridCell.clear=true;
+    gridCell.cellEl.classList.add("clear");
+    
+    for (let [drow,dcol] of directions){
+        let cellRow = row + drow;
+        let cellCol = col + dcol;
+        if (cellRow <0 || cellRow >= numRows || cellCol <0 || cellCol >= numCols){
+            continue;
+        }
+        
+        let gridCell =grid[cellRow][cellCol];
+        if (!gridCell.clear){
+            gridCell.clear=true;
+            gridCell.cellEl.classList.add("clear");
+            if(gridCell.count===0){
+                searchClearArea(grid ,cellRow,cellCol,numRows,numCols);
+            }else if (gridCell.count>0){
+                gridCell.cellEl.innerText =gridCell.count;
+            }
+        }
+    }                
+}
 
-let grid = initialize(15,20,50)
+function exploded(grid ,row,col,numRows,numCols){
+    grid[row][col].cellEl.classList.add("exploded");
 
-renderBoard(15,20,grid)
+    for (let cellRow =0; cellRow < numRows; cellRow++){
+        for (let cellCol =0; cellCol < numCols; cellCol++){
+            let cell =grid[cellRow][cellCol];
+            cell.clear =true;
+            cell.cellEl.classList.add('clear');
+
+            if (cell.count  ===-1){    //如果值为-1，则添加到有雷的类
+                cell.cellEl.classList.add('Iandmine');
+            }
+        }
+    }
+}
+
+function checkAllClear(grid){
+    for (let row=0 ;row < grid.length;row++){
+        let gridRow =grid[row];
+        for (let col =0; col <gridRow.length ;col++){
+            let cell =gridRow[col];
+            if (cell.count !=-1 && !cell.clear){
+                return false
+            }
+        }
+    }
+
+    for (let row=0 ;row < grid.length;row++){
+        let gridRow =grid[row];
+        for (let col =0; col <gridRow.length ;col++){
+            let cell =gridRow[col];
+
+            if (cell.count === -1 ){
+                cell.cellEl.classList.add("landmine");
+
+            }
+            cell.cellEl.classList.add("success");
+        
+        }
+    }
+
+    return true
+}
+let grid = initialize(9,9,15)
+
+renderBoard(9,9,grid)
